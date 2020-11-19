fefd9628f7e1020946da8f41c02c47eed84302f8
21克的爱情 <raohong07@163.com>
2020-11-19 09:52:36 +0800

    no message

--- test/ali_auth_test.dart
+++ test/ali_auth_test.dart
@@ -1,6 +1,16 @@
+/* 
+ * @Author: 21克的爱情
+ * @Date: 2020-06-18 15:27:59
+ * @Email: raohong07@163.com
+ * @LastEditors: 21克的爱情
+ * @LastEditTime: 2020-11-19 09:52:10
+ * @Description: 测试
+ */
+
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+/// 测试
 void main() {
   const MethodChannel channel = MethodChannel('ali_auth');
 
