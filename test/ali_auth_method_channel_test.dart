8889ea550b0709ec3af96839b6cbc41f2ac09b7d
21克的爱情 <raohong07@163.com>
2023-07-30 19:36:04 +0800

    update 1.1.5

--- test/ali_auth_method_channel_test.dart
+++ test/ali_auth_method_channel_test.dart
@@ -1,23 +1,23 @@
-// import 'package:flutter/services.dart';
-import 'package:flutter_test/flutter_test.dart';
-// import 'package:ali_auth/ali_auth_method_channel.dart';
-
-void main() {
-  // MethodChannelAliAuth platform = MethodChannelAliAuth();
-  // const MethodChannel channel = MethodChannel('ali_auth');
-  TestWidgetsFlutterBinding.ensureInitialized();
-
-  // setUp(() {
-  //   channel.setMockMethodCallHandler((MethodCall methodCall) async {
-  //     return '42';
-  //   });
-  // });
-  //
-  // tearDown(() {
-  //   channel.setMockMethodCallHandler(null);
-  // });
-
-  // test('getPlatformVersion', () async {
-  //   expect(await platform.getPlatformVersion(), '42');
-  // });
-}
+// // import 'package:flutter/services.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// // import 'package:ali_auth/ali_auth_method_channel.dart';
+//
+// void main() {
+//   // MethodChannelAliAuth platform = MethodChannelAliAuth();
+//   // const MethodChannel channel = MethodChannel('ali_auth');
+//   TestWidgetsFlutterBinding.ensureInitialized();
+//
+//   // setUp(() {
+//   //   channel.setMockMethodCallHandler((MethodCall methodCall) async {
+//   //     return '42';
+//   //   });
+//   // });
+//   //
+//   // tearDown(() {
+//   //   channel.setMockMethodCallHandler(null);
+//   // });
+//
+//   // test('getPlatformVersion', () async {
+//   //   expect(await platform.getPlatformVersion(), '42');
+//   // });
+// }
