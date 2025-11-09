42a8df84fe4cc4d83ab7af6c3dbf748c05a23b32
21克的爱情 <raohong07@163.com>
2025-11-09 09:02:56 +0800

    fix

---
+++ android/libs/deploy.sh
@@ -0,0 +1,3 @@
+mvn deploy:deploy-file -Dfile=sdk.aar -Durl="file://." -DgroupId="com.pgyer" -DartifactId="sdk" -Dversion="3.0.9"
+
+mvn deploy:deploy-file -Dfile=sdk.aar -Durl="file://." -DgroupId="com.mobile.auth" -DartifactId="auth-number-product" -Dversion="2.14.14"
